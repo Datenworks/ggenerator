403a5bbac5a8f6dcc93aebf48605c2da62b4e04e
Richard Lopes <richard.lopes@datenworks.com>
2020-02-18 16:25:52 -0300

    Receive a file_path, not a file_name

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -3,17 +3,17 @@ import click
 
 @click.command()
 @click.version_option(version="0.0.1")
-@click.option('-f', '--file-name', 'filename',
+@click.option('-f', '--file-path', 'filepath',
               type=click.Path(exists=True,
                               resolve_path=True,
                               file_okay=True,
                               dir_okay=False),
               required=True, prompt=True)
-def execute(filename):
-    for generated_file in generate(filename):
+def execute(filepath):
+    for generated_file in generate(filepath):
         click.echo(f"Finished, Saved on path: {generated_file}")
 
 
-def generate(filename):
+def generate(filepath):
     # TODO
     yield
