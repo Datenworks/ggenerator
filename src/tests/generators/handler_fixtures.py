a5db47d8915e48ce63ba58c77f262d8e4efcd029
Lucas Fernando <lucasfrabreu@gmail.com>
2020-02-18 19:03:58 -0300

    Add more unit test for different types of data

--- src/tests/generators/handler_fixtures.py
+++ src/tests/generators/handler_fixtures.py
@@ -2,11 +2,50 @@ from pytest import fixture
 
 
 @fixture
-def simple_valid_specification():
+def simple_specification():
     return {'size': 100,
             'fields': [{'name': 'id',
                         'type': 'integer:sequence',
-                        'generator': {}},
+                        'generator': {},
+                        'expected': 'int64'},
                        {'name': 'text',
                         'type': 'string',
-                        'generator': {}}]}
+                        'generator': {},
+                        'expected': 'object'}]}
+
+
+@fixture
+def argumented_specification():
+    return {'size': 200,
+            'fields': [{'name': 'id',
+                        'type': 'integer:sequence',
+                        'generator': {'start_at': 10,
+                                      'step': 2},
+                        'expected': 'int64'},
+                       {'name': 'date',
+                        'type': 'timestamp',
+                        'generator': {'start_at': "2000-01-01T00:00:00UTC"},
+                        'expected': 'datetime64[ns]'}]}
+
+
+def sample(type, expected):
+    return {'size': 200,
+            'fields': [{'name': 'id',
+                        'type': type,
+                        'generator': {},
+                        'expected': expected}]}
+
+
+@fixture
+def integer_specification():
+    return sample('integer', 'int64')
+
+
+@fixture
+def bool_specification():
+    return sample('boolean', 'bool')
+
+
+@fixture
+def char_specification():
+    return sample('char', 'object')
--- src/tests/generators/test_handler.py
+++ src/tests/generators/test_handler.py
@@ -1,7 +1,9 @@
 from pandas import DataFrame
 
 from src.generators.handler import GeneratorsHandler
-from src.tests.generators.handler_fixtures import simple_valid_specification
+from src.tests.generators.handler_fixtures import simple_specification, \
+    argumented_specification, integer_specification, bool_specification, \
+    char_specification
 
 
 class TestGeneratorsHandler(object):
@@ -9,11 +11,64 @@ class TestGeneratorsHandler(object):
 
     def test_simple_specification_dataframe(self,
                                             mocker,
-                                            simple_valid_specification):
+                                            simple_specification):
         mock = mocker.patch \
                      .object(GeneratorsHandler, 'get_valid_specification')
-        mock.return_value = simple_valid_specification
+        mock.return_value = simple_specification
         handler = GeneratorsHandler({'config_file': None})
         dataframe = handler.generate_dataframe()
 
         assert isinstance(dataframe, DataFrame) is True
+        assert dataframe.shape[0] == simple_specification['size']
+        for field in simple_specification['fields']:
+            assert dataframe[field['name']].dtype.name == field['expected']
+
+    def test_argumented_specification_dataframe(self,
+                                                mocker,
+                                                argumented_specification):
+        mock = mocker.patch \
+                     .object(GeneratorsHandler, 'get_valid_specification')
+        mock.return_value = argumented_specification
+        handler = GeneratorsHandler({'config_file': None})
+        dataframe = handler.generate_dataframe()
+
+        assert isinstance(dataframe, DataFrame) is True
+        assert dataframe.shape[0] == argumented_specification['size']
+        for field in argumented_specification['fields']:
+            assert dataframe[field['name']].dtype.name == field['expected']
+
+    def test_integer_dataframe(self, mocker, integer_specification):
+        mock = mocker.patch \
+                     .object(GeneratorsHandler, 'get_valid_specification')
+        mock.return_value = integer_specification
+        handler = GeneratorsHandler({'config_file': None})
+        dataframe = handler.generate_dataframe()
+
+        assert isinstance(dataframe, DataFrame) is True
+        assert dataframe.shape[0] == integer_specification['size']
+        for field in integer_specification['fields']:
+            assert dataframe[field['name']].dtype.name == field['expected']
+
+    def test_bool_dataframe(self, mocker, bool_specification):
+        mock = mocker.patch \
+                     .object(GeneratorsHandler, 'get_valid_specification')
+        mock.return_value = bool_specification
+        handler = GeneratorsHandler({'config_file': None})
+        dataframe = handler.generate_dataframe()
+
+        assert isinstance(dataframe, DataFrame) is True
+        assert dataframe.shape[0] == bool_specification['size']
+        for field in bool_specification['fields']:
+            assert dataframe[field['name']].dtype.name == field['expected']
+
+    def test_char_dataframe(self, mocker, char_specification):
+        mock = mocker.patch \
+                     .object(GeneratorsHandler, 'get_valid_specification')
+        mock.return_value = char_specification
+        handler = GeneratorsHandler({'config_file': None})
+        dataframe = handler.generate_dataframe()
+
+        assert isinstance(dataframe, DataFrame) is True
+        assert dataframe.shape[0] == char_specification['size']
+        for field in char_specification['fields']:
+            assert dataframe[field['name']].dtype.name == field['expected']
