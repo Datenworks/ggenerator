bfda0fcc6325e589118f2cadb3f95210f85b95bc
Matheus Oliveira <matheus.oliveira@datenworks.com>
2020-02-18 13:46:31 -0300

    refactor(validator): Structuring for return of true or false in the functions

--- validate.py
+++ validate.py
@@ -8,45 +8,47 @@ class Dataset(object):
         self.file = filepath
 
     def get_parameters(self):
-        files = [open(self.file, 'r').read()]   
-        for spec in files:
-           self.parameters = json.loads(spec)
+        files = open(self.file, 'r').read()
+        self.parameters = json.loads(files)
         for id in self.parameters.get('datasets').keys():
             dataset = self.parameters.get('datasets').get(id)
         return dataset
 
-class Dataset_Size_Validator(object):
 
-    def __init__ (self, dataset):
-        self.dataset = dataset
+class DatasetSizeValidator(object):
+
+    def __init__(self, dataset):
+        self.size = dataset['size']
 
     def verify_size(self):
-        if 'size' in self.dataset:
-            if isinstance(self.dataset.get('size'), int) and self.dataset.get('size') > 0:
-                self.size = self.dataset.get('size')
+        if 'size' in dataset:
+            if isinstance(self.size, int) and self.size > 0:
+                return True
             else:
                 raise "Size must be integer and higher than 0"
         else:
             raise "Dont Found Size"
 
-class Dataset_Format_Validator(object):
 
-    def __init__ (self, dataset):
-        self.dataset = dataset
+class DatasetFormatValidator(object):
+
+    def __init__(self, dataset):
+        self.format = dataset["format"]
+        self.format_header = self.format["header"]
+        self.format_type = self.format["type"]
 
     def verify_format(self):
-        if 'format' in self.dataset:
-            if 'header' in self.dataset.get('format'):
-                if isinstance(self.dataset.get('format').get('header'), bool):
-                    self.format_header = self.dataset.get('format').get('header')
+        if 'format' in dataset:
+            if 'header' in self.format:
+                if isinstance(self.format_header, bool):
+                    return True
                 else:
                     raise "Format Header must be boolean"
             else:
                 raise "Dont Found Header"
-
-            if 'type' in self.dataset.get('format'):
-                if isinstance(self.dataset.get('format').get('type'),str):
-                    self.format_type = dataset.get('format').get('type')
+            if 'type' in self.format:
+                if isinstance(self.format_type, str):
+                    return True
                 else:
                     raise "Format Type must be csv or json"
             else:
@@ -54,79 +56,78 @@ class Dataset_Format_Validator(object):
         else:
             raise "Dont Found Format"
 
-class Dataset_Fields_Validator(object):
+
+class DatasetFieldsValidator(object):
 
     def __init__(self, dataset):
-        self.dataset = dataset
+        self.field = dataset["fields"]
 
     def verify_fields(self):
-        if 'fields' in self.dataset:
-            self.field = dataset.get('fields')
-            body =  self.field
-            for body_field in body:
+        if 'fields' in dataset:
+            for body_field in self.field:
                 if 'name' in body_field:
-                    if isinstance(body_field.get('name'), str):
-                        self.field_name = body_field.get('name')
+                    self.name = body_field.get('name')
+                    if isinstance(self.name, str):
+                        return True
                     else:
                         raise "Name type must be string"
                 else:
                     raise "Dont Found Name in Fields"
                 if 'generator' in body_field:
-                    if isinstance(body_field.get('generator'), dict):
-                        self.field_generator = body_field.get('generator')
+                    self.generator = body_field.get('generator')
+                    if isinstance(self.generator, dict):
+                        return True
                     else:
                         raise "Generator must be dict"
                 else:
                     raise "Dont Found Generator in Fields"
                 if 'type' in body_field:
-                    if isinstance(body_field.get('type'), (str, bool, float, int, list)):
-                        self.field_generator = body_field.get('type')
+                    self.type = body_field.get('type')
+                    if isinstance(self.type, (str, bool, float, int, list)):
+                        return True
                     else:
                         raise "Type is not "
                 else:
                     raise "Dont Found Type in Fields"
         else:
-            raise "Dont Found Fields"
+            raise "Fields Dont Exist in Dataset"
 
-class Dataset_Serializer_Validator(object):
+
+class DatasetSerializerValidator(object):
 
     def __init__(self, dataset):
-        self.dataset = dataset
+        self.serializers = dataset["serializers"]
+        self.to = self.serializers["to"]
 
     def verify_serializers(self):
-        if 'serializers' in self.dataset:
-            if 'to' in self.dataset.get('serializers'):
-                self.serializer_to = dataset.get('serializers').get('to')
-                body_to = self.serializer_to 
-                for dict_to in body_to:
-                    if 'type' in dict_to:
-                        if isinstance(dict_to.get('type'), str):
-                            self.serializer_type = dict_to.get('type')
-                        else:
-                            raise "Serializer type must be string"
+        if 'to' in self.serializers:
+            for dict_to in self.to:
+                if 'type' in dict_to:
+                    self.type = dict_to.get('type')
+                    if isinstance(self.type, str):
+                        return True
                     else:
-                        raise "Dont Found Type in Serializers"
-                    if 'uri' in dict_to:
-                        if isinstance(dict_to.get('uri'),str):
-                            self.serializer_uri = dict_to.get('uri')
-                        else: 
-                            raise "uri error"
+                        raise "Serializer type must be string"
+                else:
+                    raise "Dont Found Type in Serializers"
+                if 'uri' in dict_to:
+                    self.to = dict_to.get('uri')
+                    if isinstance(self.to, str):
+                        return True
                     else:
-                        raise "Dont Found uri in Serializers"
-            else:
-                raise "Dont Found TO in Serializers"
-        else:
-            raise "Dont Found Serializers"
+                        raise "uri error"
+                else:
+                    raise "Dont Found uri in Serializers"
 
 
 if __name__ == "__main__":
-    
+
     spec = Dataset("fake_generator_sample.txt")
     dataset = spec.get_parameters()
-    size_validator = Dataset_Size_Validator(dataset) 
-    format_validator = Dataset_Format_Validator(dataset)
-    field_validator = Dataset_Fields_Validator(dataset)
-    serializer_validator = Dataset_Serializer_Validator(dataset) 
+    size_validator = DatasetSizeValidator(dataset)
+    format_validator = DatasetFormatValidator(dataset)
+    field_validator = DatasetFieldsValidator(dataset)
+    serializer_validator = DatasetSerializerValidator(dataset)
 
     script_name = argv[0]
     action = argv[1]
@@ -138,6 +139,6 @@ if __name__ == "__main__":
     elif action == 'field_validator':
         field_validator.verify_fields()
     elif action == 'serializer_validator':
-        serializer_validator.verify_serializers() 
+        serializer_validator.verify_serializers()
     else:
         print("Command not found")
