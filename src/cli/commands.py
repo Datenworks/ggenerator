eb365363261d0c307910529fcfa2d534f054466c
Richard Lopes <richard.lopes@datenworks.com>
2020-02-19 11:02:13 -0300

    Remove the old bootstrap cli

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -3,14 +3,16 @@ import click
 
 @click.command()
 @click.version_option(version="0.0.1")
-@click.option('-f', '--file-path', 'filepath',
+@click.option('-s', '--spec', 'spec_path',
               type=click.Path(exists=True,
                               resolve_path=True,
                               file_okay=True,
                               dir_okay=False),
               required=True, prompt=True)
-def execute(filepath):
-    for generated_file in generate(filepath):
+@click.option('-d', '--dryrun', 'dryrun',
+              is_flag=True, required=False)
+def execute(spec_path, dryrun_flag):
+    for generated_file in generate(spec_path):
         click.echo(f"Finished, Saved on path: {generated_file}")
 
 
