c1294ea92d2303635d23aff7e9b0644f1b426c9a
Lucas Fernando <lucasfrabreu@gmail.com>
2020-02-20 17:33:30 -0300

    Fix all broken unit tests

--- src/tests/cli/test_cli.py
+++ src/tests/cli/test_cli.py
@@ -1,10 +1,19 @@
 from click.testing import CliRunner
+
 from src.cli.commands import execute
+from src.generators.handler import GeneratorsHandler
 
 
 class TestCliExecutor:
     def test_cli_executor_file(self, mocker):
+        mock = mocker.patch.object(GeneratorsHandler, 'generate')
+        mock_ = mocker.patch.object(GeneratorsHandler,
+                                    'get_valid_specification')
+        mock.return_value = [("a", "b", "c",)]
+        mock_.return_value = {}
         runner = CliRunner()
-        mock = mocker.patch.Object()
-        result = runner.invoke(['spec_path'])
-        assert result.exit_code == 0
+        with runner.isolated_filesystem():
+            with open('hello.json', 'w') as f:
+                f.write("{{}}")
+                result = runner.invoke(execute, ['generate', 'hello.json'])
+                assert result.exit_code == 0
