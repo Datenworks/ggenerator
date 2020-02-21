47ceb2443f6e5dcc382658d02941d28b585fde96
Lucas Fernando <lucasfrabreu@gmail.com>
2020-02-21 18:18:06 -0300

    fix(typo): missing return

--- src/generators/datatypes/boolean.py
+++ src/generators/datatypes/boolean.py
@@ -23,7 +23,7 @@ class BooleanType:
         if self.value is None:
             return self.__generate_random()
         else:
-            self.value
+            return self.value
 
     def generate_records(self, num_of_records) -> list:
         return [self.generate() for _ in range(num_of_records)]
