4b63dbcb543d3278d588264133da7485bfff3f2b
Richard Lopes <richard.lopes@datenworks.com>
2020-02-21 17:37:25 -0300

    setup tools 'builder'

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="ggnerator",
     version="0.1",
-    packages=find_packages()
-)
+    packages=find_packages(),
+    include_packages_data=True,
+    install_requires=[
+        'Click',
+        'pandas',
+        'python-dateutil'
+    ],
+    entry_points="""
+    [console_scripts]
+    main
+    """)
