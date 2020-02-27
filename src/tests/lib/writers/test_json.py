019a6657f37cf0c3af639cac9266479dd1cc9e0c
Richard Lopes <richard.lopes@datenworks.com>
2020-02-27 13:23:14 -0300

    Fix the bug to default json writer be oriented by records, instead of columns
    Fix the tests and add two more:
    1 Case when no orient is given
    1 Case when orient is columns
    1 Case when orient is records

--- src/tests/lib/writers/test_json.py
+++ src/tests/lib/writers/test_json.py
@@ -19,7 +19,43 @@ class TestJsonWriter:
         assert isfile(test_file_path) is True
 
         with open(test_file_path) as json_file:
+            expected = pandas_dataframe_with_data.to_json(orient="records")
+
+            assert json_file.read() == expected
+
+        remove(test_file_path)
+        assert exists(test_file_path) is False
+
+    def test_writting_dataframe_with_records_orient_columns(self,
+                                             pandas_dataframe_with_data,
+                                             test_file_path):
+        json_writer = JsonWriter()
+        json_writer.write(dataframe=pandas_dataframe_with_data,
+                          file_path=test_file_path,
+                          orient="columns")
+        assert exists(test_file_path) is True
+        assert isfile(test_file_path) is True
+
+        with open(test_file_path) as json_file:
             expected = pandas_dataframe_with_data.to_json(orient="columns")
+
+            assert json_file.read() == expected
+
+        remove(test_file_path)
+        assert exists(test_file_path) is False
+
+    def test_writting_dataframe_with_records_orient_records(self,
+                                             pandas_dataframe_with_data,
+                                             test_file_path):
+        json_writer = JsonWriter()
+        json_writer.write(dataframe=pandas_dataframe_with_data,
+                          file_path=test_file_path,
+                          orient="records")
+        assert exists(test_file_path) is True
+        assert isfile(test_file_path) is True
+
+        with open(test_file_path) as json_file:
+            expected = pandas_dataframe_with_data.to_json(orient="records")
 
             assert json_file.read() == expected
 
