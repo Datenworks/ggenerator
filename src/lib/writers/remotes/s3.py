caab8f5ba709d4285213d9926009fe1b6dbc056b
Lucas Fernando <lucasfrabreu@gmail.com>
2020-02-28 17:46:48 -0300

    feat(writers): create remote s3 writers

---
+++ src/lib/writers/remotes/s3.py
@@ -0,0 +1,15 @@
+from pandas import DataFrame
+
+
+class S3RemoteWriter(object):
+    key = 's3'
+
+    def __init__(self, formatter, specification):
+        self.formatter = formatter
+        self.specification = specification
+
+    def write(self, dataframe: DataFrame) -> None:
+        options = self.specification['options']
+        key = f's3://{options["bucket"]}/{options["key"]}'
+        self.formatter.format(dataframe=dataframe,
+                              path_or_buffer=key)
