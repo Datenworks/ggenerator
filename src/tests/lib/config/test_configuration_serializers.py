3f9d222cfabe0ad9855f7f23e89e40ec5fc6943d
Lucas Fernando <lucasfrabreu@gmail.com>
2020-03-03 14:55:01 -0300

    fix(coverage): improved coverage

--- src/tests/lib/config/test_configuration_serializers.py
+++ src/tests/lib/config/test_configuration_serializers.py
@@ -1,26 +1,32 @@
 from src.lib.config.validator import ConfigurationSerializer
+from src.tests.lib.config.fixtures import *  # noqa: F403, F401
 
 
 class TestConfigurationSerializers(object):
 
     def test_valid_to(self):
-        format_sample = {
-            "to": [{
-                "type": "file",
-                "uri": ""
-            }
-            ]
-        }
+        format_sample = {"to": [{"type": "file", "uri": ""}]}
         validator = ConfigurationSerializer(format_sample)
         is_valid = validator.is_valid()
         assert is_valid is True
 
-    def test_invalid_to(self):
-        format_sample = {
-            "to": [{
+    def test_valid_s3(self, valid_s3_spec):
+        validator = ConfigurationSerializer(valid_s3_spec)
+        is_valid = validator.is_valid()
+        assert is_valid is True
 
-            }]
-        }
+    def test_invalid_s3(self, invalid_s3_spec):
+        validator = ConfigurationSerializer(invalid_s3_spec)
+        is_valid = validator.is_valid()
+        assert is_valid is False
+
+    def test_valid_s3_presigned_url(self, valid_s3_presigned_spec):
+        validator = ConfigurationSerializer(valid_s3_presigned_spec)
+        is_valid = validator.is_valid()
+        assert is_valid is True
+
+    def test_invalid_to(self):
+        format_sample = {"to": [{}]}
         validator = ConfigurationSerializer(format_sample)
         is_valid = validator.is_valid()
         assert is_valid is False
