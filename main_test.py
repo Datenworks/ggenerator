45a0563f8c2dd2305d3cbda49a3f1158ffb4b624
Tadeu Inocencio <tadeuif@hotmail.com>
2020-03-09 17:04:59 -0300

    fix(ASCCITABLE): Table is now working.

---
+++ main_test.py
@@ -0,0 +1,15 @@
+
+from src.cli.ascii_table import print_asciiTable
+from src.tests.generators.handler_fixtures import *  # noqa: F403, F401
+
+
+
+
+
+def main():
+
+    
+    print_asciiTable()
+
+
+main()
--- src/cli/ascii_table.py
+++ src/cli/ascii_table.py
@@ -1,17 +1,50 @@
 from tabulate import tabulate
+from faker import Faker
+from src.generators.datatypes import basic_generators, primitive_generators
 
 
-def print_asciiTable(dataSet):
-    for key in dataSet.keys():
-        # id = key
-        nameSpace = dataSet[key].get('fields')
-        type = dataSet[key].get('size')
-        sample = dataSet[key].get('format')
-        parameters = dataSet[key].get('serializers')
+def print_asciiTable():
 
-        newDict = {
-            "NameSpace": nameSpace,
-            "Type": type,
-            "Sample": sample,
-            "Parameters": parameters}
-    print(tabulate(newDict, headers=("keys")))
+    dataSet = {"datasets": {
+        "NameSpace": [],
+        "Type": [],
+        "Sample": [],
+        "Parameters": []}}
+    # faking data
+    fake = Faker('pt_BR')
+
+    # creating the lists
+    nameSpace_list = []
+    type_list = []
+    sample_list = []
+    parameters_list = []
+    # appending
+    for x in primitive_generators:
+        nameSpace_list.append("PrimitiveType")
+        type_list.append(x)
+        sample_list.append(fake.name())
+    for x in basic_generators:
+        nameSpace_list.append("BasicType")
+        type_list.append(x)
+    # setting values
+    dataSet['datasets']["NameSpace"] = nameSpace_list
+    dataSet['datasets']["Type"] = type_list
+    dataSet['datasets']["Sample"] = sample_list
+
+    nameSpace = dataSet["datasets"].get('NameSpace')
+    type = dataSet["datasets"].get('Type')
+    sample = dataSet["datasets"].get('Sample')
+    parameters = dataSet["datasets"].get('Parameters')
+
+    newDict = {
+        "NameSpace": nameSpace,
+        "Type": type,
+        "Sample": sample,
+        "Parameters": parameters}
+    print("   ")
+    print("   ")
+    print("Available generators")
+    print("   ")
+    print(tabulate(newDict, headers=("keys"), tablefmt="grid"))
+    print("   ")
+    print("   ")
