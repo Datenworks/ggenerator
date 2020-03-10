3f9650469124fc6827c38d4e91ccce60d5c3d8d2
Tadeu Inocencio <tadeuif@hotmail.com>
2020-03-10 17:56:41 -0300

    fix(ASCIITABLE):Some deleted files

--- src/tests/cli/test_ascii_table.py
+++
@@ -1,7 +0,0 @@
-from src.tests.generators.handler_fixtures import *  # noqa: F403, F401
-from src.cli.ascii_table import print_asciiTable
-
-
-class AsciiTester(object):
-    def test_ascii_table(self, valid_specification):
-        print_asciiTable(valid_specification)
