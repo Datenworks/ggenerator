26c3483a5b313d8920ceb7734b1e1126d3a78df0
Lucas Fernando <lucasfrabreu@gmail.com>
2020-03-12 20:07:06 -0300

    Update base handler to work with new inspector to validate the configuration file

--- src/generators/basehandler.py
+++ src/generators/basehandler.py
@@ -1,39 +1,59 @@
+import json
+
 from pandas import DataFrame, Series
 
 from src.generators.datatypes import get_generators_map
-from src.lib.config.validator import ConfigurationValidator, \
-    ConfigurationDataset
+from src.lib.config import general_rules
+from src.lib.config.fields import FieldsConfiguration
+from src.lib.config.formatters import FormattersConfiguration
+from src.lib.config.inspector import ConfigurationInpector
+from src.lib.config.serializers import SerializersConfiguration
 
 
 class BaseHandler(object):
+    def __init__(self):
+        self.inspector = ConfigurationInpector()
+
+    def __validate(self, clazz, iterable):
+        for item in iterable:
+            rules = clazz.get_rules(item)
+            self.inspector.inspect_rules(rules=rules['required'],
+                                         configuration=item)
+            self.inspector.inspect_rules(rules=rules['optional'],
+                                         configuration=item,
+                                         optional=True)
+
+    def __validate_fields(self, dataset):
+        fields = dataset['fields']
+        self.__validate(clazz=FieldsConfiguration,
+                        iterable=fields)
+
+    def __validate_format(self, dataset):
+        self.__validate(clazz=FormattersConfiguration,
+                        iterable=[dataset['format']])
+
+    def __validate_serializers(self, dataset):
+        serializers = dataset['serializers']['to']
+        self.__validate(clazz=SerializersConfiguration,
+                        iterable=serializers)
 
     def valid_specification(self, file_path):
-        config_reader = ConfigurationValidator(file_path)
-        config = config_reader.get_config()
-
-        if 'datasets' not in config:
-            raise ValueError("Don't have any datasets")
-
-        datasets = config.get('datasets')
-
-        if not datasets.keys():
-            raise ValueError("Malformed specification file")
-
-        for key in datasets.keys():
-            dataset_validator = ConfigurationDataset(
-                id=key,
-                size=datasets[key].get('size'),
-                locale=datasets[key].get('locale'),
-                fields=datasets[key].get('fields'),
-                format=datasets[key].get('format'),
-                serializers=datasets[key].get('serializers')
-            )
+        with open(file_path, 'r') as file:
             try:
-                dataset_validator.is_valid()
-            except ValueError as e:
-                raise ValueError("Malformed specification file: ", e)
+                configuration = json.loads(file.read())
+            except Exception:
+                raise ValueError("Malformed specification file/blank field")
 
-        return config
+        self.inspector.inspect_rules(rules=general_rules,
+                                     configuration=configuration)
+
+        for dataset_key in configuration['datasets']:
+            dataset = configuration['datasets'][dataset_key]
+
+            self.__validate_fields(dataset)
+            self.__validate_format(dataset)
+            self.__validate_serializers(dataset)
+        return configuration
 
     def generate_dataframe(self, specification: dict, size) -> DataFrame:
         size = size
@@ -55,3 +75,10 @@ class BaseHandler(object):
             dataframe[field_name] = series.values
 
         return dataframe
+
+
+if __name__ == "__main__":
+    import sys
+
+    handler = BaseHandler()
+    handler.valid_specification(file_path=sys.argv[1])
---
+++ src/lib/config/__init__.py
@@ -0,0 +1,8 @@
+general_rules = {
+    "datasets.{*}.size": {'type': int, 'none': False},
+    "datasets.{*}.fields.[*].type": {'type': str, 'none': False},
+    "datasets.{*}.fields.[*].name": {'type': str, 'none': False},
+    "datasets.{*}.fields.[*].generator": {'type': dict, 'none': False},
+    "datasets.{*}.format.type": {'type': str, 'none': False},
+    "datasets.{*}.serializers.to.[*].type": {'type': str, 'none': False}
+}
