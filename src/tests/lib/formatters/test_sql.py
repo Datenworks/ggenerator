488263186292b7efd6cf58d5e17cf7ad40d7a107
Richard Lopes <richard.lopes@datenworks.com>
2020-04-09 16:12:18 -0300

    First commit

---
+++ src/tests/lib/formatters/test_sql.py
@@ -0,0 +1,48 @@
+from io import StringIO
+
+from src.lib.formatters.sql import SQLFormatter
+from src.tests.lib.formatters.fixtures import *  # noqa: F403, F401
+
+
+class TestSqlFormatter(object):
+    """Unit-test of CsvWriter class"""
+
+    def test_writting_dataframe_with_records(self,
+                                             pandas_dataframe_with_data):
+        buffer = StringIO()
+        csv_writer = SQLFormatter(specification={
+            'options': {
+                'table_name': 'mytable',
+                'batch_size': 2,
+                'schema': {
+                    'Column': {
+                        'quoted': True
+                    }
+                }
+            }
+        })
+        csv_writer.format(dataframe=pandas_dataframe_with_data,
+                          path_or_buffer=buffer)
+
+        assert isinstance(buffer.getvalue(), str) is True
+        assert len(buffer.getvalue()) > 0
+
+    def test_writting_dataframe_without_records(self,
+                                                pandas_dataframe_without_data):
+        buffer = StringIO()
+        sql_writer = SQLFormatter(specification={
+            'options': {
+                'table_name': 'mytable',
+                'batch_size': 2,
+                'schema': {
+                    'Column': {
+                        'quoted': True
+                    }
+                }
+            }
+        })
+        sql_writer.format(dataframe=pandas_dataframe_without_data,
+                          path_or_buffer=buffer)
+
+        assert isinstance(buffer.getvalue(), str) is True
+        assert len(buffer.getvalue()) == 0
