99d39aeaaf2174bd07fa51ebff9b8cf99e638d82
Matheus Oliveira <matheus.oliveira@datenworks.com>
2020-04-13 15:00:40 -0300

    Fix conflicts about name SQLFormatter in lower case

--- src/lib/formatters/__init__.py
+++ src/lib/formatters/__init__.py
@@ -1,9 +1,9 @@
 from src.lib.formatters.csv import CsvFormatter
 from src.lib.formatters.json import JsonFormatter
-from src.lib.formatters.sql import SqlFormatter
+from src.lib.formatters.sql import SQLFormatter
 
 formatters = {
     CsvFormatter.key: CsvFormatter,
     JsonFormatter.key: JsonFormatter,
-    SqlFormatter.key: SqlFormatter
+    SQLFormatter.key: SQLFormatter
 }
