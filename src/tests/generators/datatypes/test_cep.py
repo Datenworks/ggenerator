a31fff5174fcf0e0f88de6015ba21243e6395e96
Tadeu Inocencio <tadeuif@hotmail.com>
2020-04-14 12:35:25 -0300

    Some tests is working

---
+++ src/tests/generators/datatypes/test_cep.py
@@ -0,0 +1,19 @@
+from src.generators.datatypes.cep import CepProvider
+
+
+class TestCep:
+    def test_cep_with_mask(self):
+        cepProvider = CepProvider()
+        cep = cepProvider.cep(True)
+        assert isinstance(cep, str)
+        assert cep[5] == '-'
+        assert len(cep) == 9
+        assert cepProvider().locale == 'pt_BR'
+
+    def test_cep_without_mask(self):
+        cepProvider = CepProvider()
+        cep = cepProvider.cep(False)
+        assert isinstance(cep, str)
+        assert cep[5] != '-'
+        assert len(cep) == 8
+        assert cepProvider().locale == 'pt_BR'
