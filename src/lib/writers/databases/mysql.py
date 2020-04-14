a4616681a55014efc3b78663be790617ea076718
Lucas Fernando <lucasfrabreu@gmail.com>
2020-04-14 18:25:49 -0300

    feat(mysql_writer): finished implementation of mysql writer

--- src/lib/writers/databases/mysql.py
+++ src/lib/writers/databases/mysql.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 from pandas import DataFrame
 
 from src.lib.mysql.connection import MysqlConnection
@@ -11,7 +12,7 @@ class MysqlDatabaseWriter(object):
         self.specification = specification
 
     @staticmethod
-    def rules(self):
+    def rules():
         return {
             'required': {
                 'options.host': {'none': False, 'type': str},
@@ -24,11 +25,31 @@ class MysqlDatabaseWriter(object):
             }
         }
 
+    def before_write(self):
+        host = self.specification \
+                   .get('options') \
+                   .get('host')
+        user = self.specification \
+                   .get('options') \
+                   .get('username')
+        print(f"Database host: {host}")
+        print(f"Username: {user}")
+        self.specification['options']['password'] = \
+            getpass(f"Type {user} password: ")
+
     def write(self, dataframe: DataFrame):
+        row_count = 0
         parameters = self.specification \
                          .get('options')
 
-        with MysqlConnection(**parameters) as connection:
-            query = self.formatter \
-                        .format(dataframe=dataframe)
-            return connection.execute_query(query)
+        sql = self.formatter \
+                  .format(dataframe=dataframe,
+                          path_or_buffer=None)
+        if sql is not None:
+            with MysqlConnection(**parameters) as connection:
+                for query in sql.split(';'):
+                    if len(query) < 6:
+                        continue
+                    row_count += connection.execute_query(query)
+
+        return f"{row_count} rows inserted"
