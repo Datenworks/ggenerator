163e0719835b9121e0ed651ee0acc829ee0e75db
Richard Lopes <richard.lopes@datenworks.com>
2020-04-15 12:17:19 -0300

    Fix namespace and locale for providers

--- src/generators/datatypes/faker_custom/ssn/pt_BR/__init__.py
+++ src/generators/datatypes/faker_custom/ssn/pt_BR/__init__.py
@@ -2,6 +2,8 @@ from faker.providers.ssn.pt_BR import Provider as SsnProvider
 
 
 class DocumentProvider(SsnProvider):
+    namespace = 'ssn'
+
     def titulo_eleitoral(self, mask: bool = True):
         if mask:
             return self.bothify('#### #### ####')
