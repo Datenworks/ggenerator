0d0fa6a52b72513e170662ef185aaa97873d50c5
Tadeu Inocencio <tadeuif@hotmail.com>
2020-04-15 13:24:02 -0300

    getting even with the develop branch

--- src/generators/datatypes/faker_custom/__init__.py
+++ src/generators/datatypes/faker_custom/__init__.py
@@ -1,8 +1,5 @@
-from src.generators.datatypes.\
-    faker_custom.pt_BR.cep import CepProvider
-from src.generators.datatypes.\
-    faker_custom.pt_BR.ssn import DocumentProvider
-
+from src.generators.datatypes.faker_custom.address.pt_BR import CepProvider
+from src.generators.datatypes.faker_custom.ssn.pt_BR import DocumentProvider
 
 custom_providers = {
     'pt_BR': [DocumentProvider, CepProvider]
--- src/tests/generators/datatypes/test_cep.py
+++ src/tests/generators/datatypes/test_cep.py
@@ -1,17 +1,17 @@
-from src.generators.datatypes.faker_custom.pt_BR.cep import CepProvider
+from src.generators.datatypes.faker_custom.address.pt_BR import CepProvider
 
 
 class TestCep:
     def test_cep_with_mask(self):
-        cepProvider = CepProvider('address')
-        cep = cepProvider.postcode(True)
+        cepProvider = CepProvider()
+        cep = cepProvider.postcode(mask=True)
         assert isinstance(cep, str)
         assert cep[5] == '-'
         assert len(cep) == 9
 
     def test_cep_without_mask(self):
-        cepProvider = CepProvider('address')
-        cep = cepProvider.postcode(False)
+        cepProvider = CepProvider()
+        cep = cepProvider.postcode(mask=False)
         assert isinstance(cep, str)
         assert cep[5] != '-'
         assert len(cep) == 8
