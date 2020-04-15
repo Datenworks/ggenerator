67bde5a7b13ce9a8d4e41748425e0281242f790a
Matheus Oliveira <matheus.oliveira@datenworks.com>
2020-04-15 12:24:23 -0300

    Create a generate CNH

--- src/generators/datatypes/faker_custom/pt_BR/documents.py
+++ src/generators/datatypes/faker_custom/pt_BR/documents.py
@@ -2,8 +2,8 @@ from faker.providers.ssn.pt_BR import Provider as SsnProvider
 
 
 class DocumentProvider(SsnProvider):
-    def titulo_eleitoral(self, mask: bool = True):
+    def cnh(self, mask: bool = True):
         if mask:
-            return self.bothify('#### #### ####')
+            return self.bothify('### ### ### ##')
         else:
-            return self.bothify('############')
+            return self.bothify('###########')
