37fa8e58984e31d8f776a3ebe706c6084c12f8ce
Tadeu Inocencio <tadeuif@hotmail.com>
2020-04-16 12:54:36 -0300

    postgres connection and direct files

--- src/lib/writers/databases/postgres.py
+++ src/lib/writers/databases/postgres.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from getpass import getpass
+from pandas import DataFrame
 
 
 class PostgresDataBaseWritter(object):
@@ -30,3 +31,42 @@ class PostgresDataBaseWritter(object):
         print(f"Database username: {user}")
         self.specification['options']['password'] = \
             getpass(f"Type {user} password: ")
+
+
+class PostgresDirectDatabaseWriter(PostgresDataBaseWritter):
+    """Class that receive pandas dataframe
+    and write it on a SGDB
+    """
+    key = 'sql'
+
+    def __init__(self, formatter, specification):
+        self.formatter = formatter
+        self.default = {
+            'schema': None
+        }
+        self.specification = specification
+
+    def engine(self, params):
+        return create_engine('postgres+postgresconnector: //'
+                             f"{params['username']}"
+                             f"{params['password']}@"
+                             f"{params['host']}:"
+                             f"{params['port']}/"
+                             f"{params['database']}")
+
+    def write(self, dataframe: Dataframe) -> None:
+        """Write all dataframe on a DataBase Table.
+
+        Keyword arguments:
+            - dataframe - pandas.Dataframe: dataframe containing the records
+        """
+        parameters = self.default
+        options = self.specification.get('options', {})
+        parameters.update(options)
+
+        connection = self.engine(parameters)
+
+        self.formatter.format(dataframe=dataframe,
+                              path_or_buffer=connection,
+                              method='direct',
+                              schema=parameters['schema'])
