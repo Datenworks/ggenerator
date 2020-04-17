f920fd5794f7ac74c1c2dc654a27f06822edeabd
Tadeu Inocencio <tadeuif@hotmail.com>
2020-04-17 12:21:32 -0300

    Uncometing tests

--- src/tests/lib/writers/databases/test_postgres.py
+++ src/tests/lib/writers/databases/test_postgres.py
@@ -7,18 +7,18 @@ import pandas as pd
 
 
 class TestPostgresDirectDatabaseWriter(object):
-    # def test_before_write(self, mocker, sql_formatter, postgres_specification):
-    #     expected = "123"
-    #     mock = mocker.patch('getpass._raw_input')
-    #     mock.return_value = expected
+    def test_before_write(self, mocker, sql_formatter, postgres_specification):
+        expected = "123"
+        mock = mocker.patch('getpass._raw_input')
+        mock.return_value = expected
 
-    #     formatter = SQLFormatter(specification=sql_formatter)
-    #     writer = PostgresDirectDatabaseWriter(formatter=formatter,
-    #                                           specification={'options': {}})
-    #     writer.before_write()
+        formatter = SQLFormatter(specification=sql_formatter)
+        writer = PostgresDirectDatabaseWriter(formatter=formatter,
+                                              specification={'options': {}})
+        writer.before_write()
 
-    #     mock.assert_called()
-    #     assert writer.specification['options']['password'] == expected
+        mock.assert_called()
+        assert writer.specification['options']['password'] == expected
 
     def test_writting_csv_with_records(self,
                                        mocker,
@@ -45,18 +45,18 @@ class TestPostgresDirectDatabaseWriter(object):
         assert dataframe_from_sql.equals(dataframe)
         mock_.assert_called()
 
-    # def test_writting_dataframe_without_records(self,
-    #                                             mocker,
-    #                                             sql_formatter,
-    #                                             pandas_dataframe_without_data,
-    #                                             postgres_specification):
-    #     mock_conn = mocker.patch('psycopg2.connect')
-    #     mock_conn.cursor.return_value = lambda x: 0
+    def test_writting_dataframe_without_records(self,
+                                                mocker,
+                                                sql_formatter,
+                                                pandas_dataframe_without_data,
+                                                postgres_specification):
+        mock_conn = mocker.patch('psycopg2.connect')
+        mock_conn.cursor.return_value = lambda x: 0
 
-    #     formatter = SQLFormatter(specification={})
-    #     writer = PostgresDirectDatabaseWriter(
-    #         formatter=formatter,
-    #         specification=postgres_specification)
-    #     writer.write(dataframe=pandas_dataframe_without_data)
+        formatter = SQLFormatter(specification={})
+        writer = PostgresDirectDatabaseWriter(
+            formatter=formatter,
+            specification=postgres_specification)
+        writer.write(dataframe=pandas_dataframe_without_data)
 
-    #     mock_conn.assert_not_called()
+        mock_conn.assert_not_called()
